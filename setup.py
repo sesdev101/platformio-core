6a5151eb0523abe2bc1d83d258cd73d874a8ac12
sesdev101 <ses.master101@gmail.com>
2025-07-25 10:36:17 +0300

    adding comment

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+# we should do some checkings before running the setup
 
 from setuptools import find_packages, setup
 
